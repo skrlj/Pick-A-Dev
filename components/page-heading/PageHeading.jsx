f2f3d1c5a22b56ff75748de527fcf2fe343df7ae
James Skrlj <jskrlj1@nait.ca>
2022-11-11 17:15:24 -0700

    changed tagline styling

--- components/page-heading/PageHeading.jsx
+++ components/page-heading/PageHeading.jsx
@@ -4,7 +4,7 @@ function PageHeading({title, tagline, ...props}) {
             <header className="text-center">
                 <img className='mx-auto w-[30rem]' src='pickadev.png' alt='pick a dev'></img>
                 <h1 className="text-4xl md:text-6xl lg:text-8xl font-extrabold text-blue-violet py-3">{title}</h1>
-                <p className="font-mono text-sm md:text-lg lg:text-2xl text-regal-blue">{tagline}</p>
+                <p className="text-base md:text-lg lg:text-xl text-regal-blue">{tagline}</p>
             </header>
         </div>
      );

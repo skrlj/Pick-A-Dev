6d435eacbebfab484ff66f60151cb8075765a023
James Skrlj <jskrlj1@nait.ca>
2022-12-04 19:13:56 -0700

    final touches

--- components/headers/landing-page-header/LandingPageHeader.jsx
+++ components/headers/landing-page-header/LandingPageHeader.jsx
@@ -9,7 +9,7 @@ function LandingPageHeader({title, tagline, ...props}) {
                     <h1 className="lg:text-center animate-pulse text-4xl md:text-6xl lg:text-8xl font-extrabold text-cyan-100 py-3 uppercase">
                         {title}
                     </h1>
-                    <p className="lg:bg-teal-700 lg:shadow-lg  lg:p-3 lg:rounded-full text-base md:text-lg lg:text-xl text-teal-100">{tagline}</p>
+                    <p className="lg:bg-teal-700 lg:shadow-lg lg:p-3 lg:my-3 lg:rounded-full text-base md:text-lg lg:text-xl text-teal-100">{tagline}</p>
                 </figcaption>
                 <Image
                     className='w-[50rem] mx-auto p-10'
